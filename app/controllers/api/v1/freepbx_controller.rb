4ee27b384d177ca1583745acb7efe799f901636d
tahir-s <abumutahira@gmail.com>
2018-11-19 22:28:02 +0500

    extension for udp plus refactoring

--- app/controllers/api/v1/freepbx_controller.rb
+++ app/controllers/api/v1/freepbx_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::FreepbxController < ApplicationController
     require 'csv'
 
     before_action :authenticate_with_token
-    before_action :set_csv_vars, :set_uuid, only: [:update]
+    before_action :set_csv_vars, :set_uuid, :set_cmd, only: [:update]
 
     # this API is to run extension adding file run command 
     def create
@@ -42,98 +42,19 @@ class Api::V1::FreepbxController < ApplicationController
     def update
 
         CSV.open("#{@path}#{@uuid}.csv", "wb") do |csv|
-            csv << ["extension", "password", "name", "voicemail", "ringtimer", "noanswer", "recording", "outboundcid", "sipname", "noanswer_cid", "busy_cid", "chanunavail_cid", "noanswer_dest", "busy_dest", "chanunavail_dest", "mohclass", "id", "tech", "dial", "devicetype", "user", "description", "emergency_cid", "recording_in_external", "recording_out_external", "recording_in_internal", "recording_out_internal", "recording_ondemand", "recording_priority", "answermode", "intercom", "cid_masquerade", "concurrency_limit", "accountcode", "allow", "avpf", "callerid", "canreinvite", "context", "defaultuser", "deny", "disallow", "dtmfmode", "encryption", "force_avp", "host", "icesupport", "namedcallgroup", "namedpickupgroup", "nat", "permit", "port", "qualify", "qualifyfreq", "rtcp_mux", "secret", "sendrpid", "sessiontimers", "sipdriver", "transport", "trustrpid", "type", "videosupport", "voicemail_enable", "voicemail_vmpwd", "voicemail_email", "voicemail_pager", "voicemail_options", "voicemail_same_exten", "disable_star_voicemail", "vmx_unavail_enabled", "vmx_busy_enabled", "vmx_temp_enabled", "vmx_play_instructions", "vmx_option_0_number", "vmx_option_1_number", "vmx_option_2_number"]
+            csv << set_labels()
 
-            wss_or_udp_data = [ 
-                    @extension, 
-                    @password, 
-                    @name, 
-                    @voicemail, 
-                    @ringtimer, 
-                    @noanswer, 
-                    @recording, 
-                    @outboundcid, 
-                    @sipname, 
-                    @noanswer_cid, 
-                    @busy_cid, 
-                    @chanunavail_cid, 
-                    @noanswer_dest, 
-                    @busy_dest, 
-                    @chanunavail_dest, 
-                    @mohclass, 
-                    @id, 
-                    @tech, 
-                    @dial, 
-                    @devicetype, 
-                    @user, 
-                    @description, 
-                    @emergency_cid, 
-                    @recording_in_external, 
-                    @recording_out_external, 
-                    @recording_in_internal, 
-                    @recording_out_internal, 
-                    @recording_ondemand, 
-                    @recording_priority, 
-                    @answermode, 
-                    @intercom, 
-                    @cid_masquerade, 
-                    @concurrency_limit, 
-                    @accountcode, 
-                    @allow, 
-                    @avpf, 
-                    @callerid, 
-                    @canreinvite, 
-                    @context, 
-                    @defaultuser, 
-                    @deny, 
-                    @disallow, 
-                    @dtmfmode, 
-                    @encryption, 
-                    @force_avp, 
-                    @host, 
-                    @icesupport, 
-                    @namedcallgroup, 
-                    @namedpickupgroup, 
-                    @nat, 
-                    @permit, 
-                    @port, 
-                    @qualify, 
-                    @qualifyfreq, 
-                    @rtcp_mux, 
-                    @secret, 
-                    @sendrpid, 
-                    @sessiontimers, 
-                    @sipdriver, 
-                    @transport, 
-                    @trustrpid, 
-                    @type, 
-                    @videosupport, 
-                    @voicemail_enable, 
-                    @voicemail_vmpwd, 
-                    @voicemail_email, 
-                    @voicemail_pager, 
-                    @voicemail_options, 
-                    @voicemail_same_exten, 
-                    @disable_star_voicemail, 
-                    @vmx_unavail_enabled, 
-                    @vmx_busy_enabled, 
-                    @vmx_temp_enabled, 
-                    @vmx_play_instructions, 
-                    @vmx_option_0_number, 
-                    @vmx_option_1_number, 
-                    @vmx_option_2_number
-                  ]
+            wss_extension_set() # set wss extension
+            set_extension_constructed_vars() # set wss's extension based variables accrodingly
+            set_wss_options()   # set wss variables accrodingly
 
-            csv << wss_or_udp_data
+            csv << set_wss__udp_data()
 
-            wss_or_udp_data[35] = 'no'
-            wss_or_udp_data[42] = 'rfc2833'
-            wss_or_udp_data[43] = 'no'
-            wss_or_udp_data[44] = 'no'
-            wss_or_udp_data[54] = 'no'
-            wss_or_udp_data[59] = 'udp,tcp,tls'
-
-            csv << wss_or_udp_data
+            udp_extension_set() # set udp extension
+            set_extension_constructed_vars() # set udp's extension based variables accrodingly
+            set_udp_options() # set udp variables accrodingly
+            
+            csv << set_wss__udp_data()
         end
 
         puts set_cmd
@@ -143,7 +64,7 @@ class Api::V1::FreepbxController < ApplicationController
 		    render :json => { executed: system( set_cmd ) }, status: 200 
             }
         end     
-    end
+    end 
 
     private
         def authenticate_with_token
@@ -151,20 +72,13 @@ class Api::V1::FreepbxController < ApplicationController
         end
 
         def set_csv_vars
-            
             # variables passed as params
             @path                        = params[:path]
-            @extension                   = params[:extension]
             @name                        = params[:name] 
             @secret                      = params[:secret]
 
-            #constructed
-            @id                          = @extension
-            @user                        = @extension
+            # constructed
             @description                 = @name
-	    @dial                        = 'SIP/' + @extension
-            @callerid                    = @name + ' <' + @extension + '>'
-            @cid_masquerade              = @extension
 
             # constants
             @password                    = nil
@@ -195,15 +109,11 @@ class Api::V1::FreepbxController < ApplicationController
             @concurrency_limit           = '3'
             @accountcode                 = nil
             @allow                       = nil
-            @avpf                        = 'yes'
             @canreinvite                 = 'no'
             @context                     = 'from-internal'
             @defaultuser                 = nil
             @deny                        = '0.0.0.0/0.0.0.0'
             @disallow                    = nil
-            @dtmfmode                    = 'auto'
-            @encryption                  = 'yes'
-            @force_avp                   = 'yes'
             @host                        = 'dynamic'
             @icesupport                  = 'yes'
             @namedcallgroup              = nil
@@ -213,11 +123,9 @@ class Api::V1::FreepbxController < ApplicationController
             @port                        = '5060'
             @qualify                     = 'yes'
             @qualifyfreq                 = '60'
-            @rtcp_mux                    = 'yes'
             @sendrpid                    = 'pai'
             @sessiontimers               = 'accept'
             @sipdriver                   = 'chan_sip'
-            @transport                   = 'wss,udp,tcp,tls'
             @trustrpid                   = 'yes'
             @type                        = 'friend'
             @videosupport                = 'inherit'
@@ -237,11 +145,212 @@ class Api::V1::FreepbxController < ApplicationController
             @vmx_option_2_number         = nil
         end
         
+        def wss_extension_set
+            @extension                   = params[:extension]
+        end
+
+        def udp_extension_set
+            @extension                   = params[:extension] + 'udp'
+        end
+
+        def set_extension_constructed_vars()
+            # constructed
+            @id                          = @extension
+            @user                        = @extension
+	        @dial                        = 'SIP/' + @extension
+            @callerid                    = @name + ' <' + @extension + '>'
+            @cid_masquerade              = @extension
+        end
+
+        def set_wss_options()
+            @avpf                        = 'yes'
+            @dtmfmode                    = 'auto'
+            @encryption                  = 'yes'
+            @force_avp                   = 'yes'
+            @rtcp_mux                    = 'yes'
+            @transport                   = 'wss,udp,tcp,tls'
+        end
+
+        def set_udp_options()
+            @avpf                        = 'no'
+            @dtmfmode                    = 'rfc2833'
+            @encryption                  = 'no'
+            @force_avp                   = 'no'
+            @rtcp_mux                    = 'no'
+            @transport                   = 'udp,tcp,tls'
+        end
+
+        def set_wss__udp_data()
+            wss__udp_data = [ 
+                @extension, 
+                @password, 
+                @name, 
+                @voicemail, 
+                @ringtimer, 
+                @noanswer, 
+                @recording, 
+                @outboundcid, 
+                @sipname, 
+                @noanswer_cid, 
+                @busy_cid, 
+                @chanunavail_cid, 
+                @noanswer_dest, 
+                @busy_dest, 
+                @chanunavail_dest, 
+                @mohclass, 
+                @id, 
+                @tech, 
+                @dial, 
+                @devicetype, 
+                @user, 
+                @description, 
+                @emergency_cid, 
+                @recording_in_external, 
+                @recording_out_external, 
+                @recording_in_internal, 
+                @recording_out_internal, 
+                @recording_ondemand, 
+                @recording_priority, 
+                @answermode, 
+                @intercom, 
+                @cid_masquerade, 
+                @concurrency_limit, 
+                @accountcode, 
+                @allow, 
+                @avpf, 
+                @callerid, 
+                @canreinvite, 
+                @context, 
+                @defaultuser, 
+                @deny, 
+                @disallow, 
+                @dtmfmode, 
+                @encryption, 
+                @force_avp, 
+                @host, 
+                @icesupport, 
+                @namedcallgroup, 
+                @namedpickupgroup, 
+                @nat, 
+                @permit, 
+                @port, 
+                @qualify, 
+                @qualifyfreq, 
+                @rtcp_mux, 
+                @secret, 
+                @sendrpid, 
+                @sessiontimers, 
+                @sipdriver, 
+                @transport, 
+                @trustrpid, 
+                @type, 
+                @videosupport, 
+                @voicemail_enable, 
+                @voicemail_vmpwd, 
+                @voicemail_email, 
+                @voicemail_pager, 
+                @voicemail_options, 
+                @voicemail_same_exten, 
+                @disable_star_voicemail, 
+                @vmx_unavail_enabled, 
+                @vmx_busy_enabled, 
+                @vmx_temp_enabled, 
+                @vmx_play_instructions, 
+                @vmx_option_0_number, 
+                @vmx_option_1_number, 
+                @vmx_option_2_number
+            ]
+
+            return wss__udp_data
+        end
+
+        def set_labels()
+            [
+                "extension", 
+                "password", 
+                "name", 
+                "voicemail", 
+                "ringtimer", 
+                "noanswer", 
+                "recording", 
+                "outboundcid", 
+                "sipname", 
+                "noanswer_cid", 
+                "busy_cid", 
+                "chanunavail_cid", 
+                "noanswer_dest", 
+                "busy_dest", 
+                "chanunavail_dest", 
+                "mohclass", 
+                "id", 
+                "tech", 
+                "dial", 
+                "devicetype", 
+                "user", 
+                "description", 
+                "emergency_cid", 
+                "recording_in_external", 
+                "recording_out_external", 
+                "recording_in_internal", 
+                "recording_out_internal", 
+                "recording_ondemand", 
+                "recording_priority", 
+                "answermode", 
+                "intercom", 
+                "cid_masquerade", 
+                "concurrency_limit", 
+                "accountcode", 
+                "allow", 
+                "avpf", 
+                "callerid", 
+                "canreinvite", 
+                "context", 
+                "defaultuser", 
+                "deny", 
+                "disallow", 
+                "dtmfmode", 
+                "encryption", 
+                "force_avp", 
+                "host", 
+                "icesupport", 
+                "namedcallgroup", 
+                "namedpickupgroup", 
+                "nat", 
+                "permit", 
+                "port", 
+                "qualify", 
+                "qualifyfreq", 
+                "rtcp_mux", 
+                "secret", 
+                "sendrpid", 
+                "sessiontimers", 
+                "sipdriver", 
+                "transport", 
+                "trustrpid", 
+                "type", 
+                "videosupport", 
+                "voicemail_enable", 
+                "voicemail_vmpwd", 
+                "voicemail_email", 
+                "voicemail_pager", 
+                "voicemail_options", 
+                "voicemail_same_exten", 
+                "disable_star_voicemail", 
+                "vmx_unavail_enabled", 
+                "vmx_busy_enabled", 
+                "vmx_temp_enabled", 
+                "vmx_play_instructions", 
+                "vmx_option_0_number", 
+                "vmx_option_1_number", 
+                "vmx_option_2_number"
+            ]
+        end
+
         def set_uuid
             @uuid = UUIDTools::UUID.timestamp_create
         end
 
         def set_cmd
-            return "/root/script/addextension.sh #{@path}#{@uuid}.csv #{@extension}"
+            return "/root/script/addextension.sh #{@path}#{@uuid}.csv #{params[:extension]}"
         end
 end
